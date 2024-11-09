c2c5c4f2986db080117bb03137885af2d2d0e2d0
ITomchakovskiy <pm13.tomchakovsky@gmail.com>
2024-11-09 20:13:38 +0700

    добавил масштабирование сечения

--- MAIN.cpp
+++ MAIN.cpp
@@ -62,82 +62,69 @@ struct Point3DFloat
    }
    Point3DFloat ApplyChangesToVector(float* Matrix_)
    {
-      //float coef = Matrix_[3] * x + Matrix_[7] * y + Matrix_[11] * z;
       Point3DFloat result(((Matrix_[0] * x + Matrix_[4] * y + Matrix_[8] * z)),
          ((Matrix_[1] * x + Matrix_[5] * y + Matrix_[9] * z)),
          ((Matrix_[2] * x + Matrix_[6] * y + Matrix_[10] * z)));
       result.Normalize();
       return result;
-     /* return Point3DFloat(((Matrix_[0] * x + Matrix_[4] * y + Matrix_[8] * z)),
-         ((Matrix_[1] * x + Matrix_[5] * y + Matrix_[9] * z)),
-         ((Matrix_[2] * x + Matrix_[6] * y + Matrix_[10] * z)));*/
    }
    float GetAngle(Point3DFloat& other)
    {
-      double cos = (other.x*x + other.y*y + other.z*z) / (other.Norm() * this->Norm()) ; //считаем угол для поворота
+      double cos = (other.x * x + other.y * y + other.z * z) / (other.Norm() * this->Norm()); //считаем угол для поворота
       double angle_rad = acosf(cos);
       float angle_grad = angle_rad * 180. / M_PI;
       return angle_grad;
    }
 };
 
-Point3DFloat SectionVertices[5];
+//Point3DFloat SectionVertices[5];
 
-Point3DFloat SectionNormals[5];
+// SectionNormalVectors[5];
 
 vector<Point3DFloat> ReplicationPath;
-
-void SectionNormalsInit()
-{
-   Point3DFloat VecZ = Point3DFloat(0., 0., 1.);
-   for (int i = 0; i < 4; i++)
-   {
-      Point3DFloat VecSide = SectionVertices[i + 1] - SectionVertices[i];
-      SectionNormals[i] = VecZ.Cross(VecSide);
-   }
-   Point3DFloat VecSide = SectionVertices[0] - SectionVertices[4];
-   SectionNormals[4] = VecZ.Cross(VecSide);
-}
 
 struct Point3DFloatSet
 {
    Point3DFloat Points[5];
    Point3DFloatSet() {};
-   Point3DFloatSet(Point3DFloat *points)
-   {
-      *this = points;
-   }
+ 
    void ApplyChangesToPointsSet(float* Matrix_)
    {
       for (int i = 0; i < 5; i++)
       {
-         Point3DFloat Point = SectionVertices[i];
-         float coef = Matrix_[3] * Point.x + Matrix_[7] * Point.y + Matrix_[11] * Point.z + Matrix_[15];
-         /*Points[i] = Point3DFloat(((Matrix_[0] * Point.x + Matrix_[4] * Point.y + Matrix_[8] * Point.z + Matrix_[12]) / coef),
-                                   ((Matrix_[1] * Point.x + Matrix_[5] * Point.y + Matrix_[9] * Point.z + Matrix_[13]) / coef),
-                                   ((Matrix_[2] * Point.x + Matrix_[6] * Point.y + Matrix_[10] * Point.z + Matrix_[14]) / coef));*/
-         Points[i] = Point.ApplyChangesToPoint(Matrix_);
+         Points[i] = Points[i].ApplyChangesToPoint(Matrix_);
       }
    }
    void ApplyChangesToVectorsSet(float* Matrix_)
    {
       for (int i = 0; i < 5; i++)
       {
-         Point3DFloat Vector = SectionNormals[i];
-         //float coef = Matrix_[3] * Vector.x + Matrix_[7] * Vector.y + Matrix_[11] * Vector.z;
-         /*Points[i] = Point3DFloat(((Matrix_[0] * Vector.x + Matrix_[4] * Vector.y + Matrix_[8] * Vector.z)),
-                                  ((Matrix_[1] * Vector.x + Matrix_[5] * Vector.y + Matrix_[9] * Vector.z)),
-                                  ((Matrix_[2] * Vector.x + Matrix_[6] * Vector.y + Matrix_[10] * Vector.z)));*/
-         Points[i] = Vector.ApplyChangesToVector(Matrix_);
+         Points[i] = Points[i].ApplyChangesToVector(Matrix_);
       }
    }
 };
 
+Point3DFloatSet SectionVertices;
+
+Point3DFloatSet SectionNormalVectors;
+
 vector<Point3DFloatSet> Points;
 
-vector<Point3DFloatSet> Normals;
+vector<Point3DFloatSet> NormalVectors;
 
-Point3DFloat BorderNormals[2];
+Point3DFloat BorderNormalVectors[2];
+
+void SectionNormalVectorsInit()
+{
+   Point3DFloat VecZ = Point3DFloat(0., 0., 1.);
+   for (int i = 0; i < 4; i++)
+   {
+      Point3DFloat VecSide = SectionVertices.Points[i + 1] - SectionVertices.Points[i];
+      SectionNormalVectors.Points[i] = VecZ.Cross(VecSide);
+   }
+   Point3DFloat VecSide = SectionVertices.Points[0] - SectionVertices.Points[4];
+   SectionNormalVectors.Points[4] = VecZ.Cross(VecSide);
+}
 
 void ReadSection()
 {
@@ -145,8 +132,8 @@ void ReadSection()
    fin.open("Section.txt", ios::in);
    for (int i = 0; i < 5; i++)
    {
-      fin >> SectionVertices[i].x;
-      fin >> SectionVertices[i].y;
+      fin >> SectionVertices.Points[i].x;
+      fin >> SectionVertices.Points[i].y;
    }
    fin.close();
 }
@@ -164,113 +151,147 @@ void ReadReplicationPath()
       fin >> ReplicationPath[i].y;
       fin >> ReplicationPath[i].z;
    }
+   fin.close();
+}
+
+vector<std::pair<float,float>> ScaleModifiers;
+
+void ReadScaleModifiers()
+{
+   fstream fin;
+   fin.open("ScaleModifiers.txt", ios::in);
+   int n = ReplicationPath.size();
+   ScaleModifiers.resize(n);
+   for (int i = 0; i < n; i++)
+   {
+      fin >> ScaleModifiers[i].first;
+      fin >> ScaleModifiers[i].second;
+   }
+   fin.close();
+}
+
+void RotateVectorToVector(Point3DFloat& A, Point3DFloat& B)
+{
+   float angle = A.GetAngle(B);
+   Point3DFloat RotateDirection = B.Cross(A); //вектор,вокруг которого будем совершать поворот
+   if (RotateDirection.x > 1.0E-5 || RotateDirection.y > 1.0E-5 || RotateDirection.z > 1.0E-5)
+   {
+      RotateDirection.Normalize();
+      glRotatef(angle, RotateDirection.x, RotateDirection.y, RotateDirection.z);  //поворот, ось z по направлению тиражирования
+   }
 }
 
 float Matrix[16];
 
+void ModifyScale(Point3DFloatSet& SectionPoints_, Point3DFloatSet& SectionNormalVectors_, float* Matrix_, int num)
+{
+   glPushMatrix(); //загрузка матрицы в стек
+
+   glScalef(ScaleModifiers[num].first, ScaleModifiers[num].second, 1.);
+
+   glGetFloatv(GL_MODELVIEW_MATRIX, Matrix_); //получаем матрицу преобразований
+
+   glPopMatrix();
+
+   SectionPoints_.ApplyChangesToPointsSet(Matrix_);
+   SectionNormalVectors_.ApplyChangesToVectorsSet(Matrix_);
+}
+
 void FirstSection()
 {
-   //glPushMatrix(); //загрузка матрицы в стек
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+   glPushMatrix();
+
+   Point3DFloatSet SectionPointsWithModifiedScale = SectionVertices;
+   Point3DFloatSet SectionNormalVectorsWithModifiedScale = SectionNormalVectors;
+   ModifyScale(SectionPointsWithModifiedScale, SectionNormalVectorsWithModifiedScale, Matrix, 0);
+
    glTranslatef(ReplicationPath[0].x, ReplicationPath[0].y, ReplicationPath[0].z); //перемещение в начало тиражирования
+
    Point3DFloat ReplicationDirection = ReplicationPath[1] - ReplicationPath[0];  //направление тиражирования
    Point3DFloat SectionNormal = Point3DFloat(0, 0, 1.); //нормаль
-   float angle = ReplicationDirection.GetAngle(SectionNormal);
-   Point3DFloat RotateDirection = SectionNormal.Cross(ReplicationDirection); //вектор,вокруг которого будем совершать поворот
-   if (RotateDirection.x > 1.0E-5 || RotateDirection.y > 1.0E-5 || RotateDirection.z > 1.0E-5)
-   {
-      RotateDirection.Normalize();
-      glRotatef(angle, RotateDirection.x, RotateDirection.y, RotateDirection.z);  //поворот, ось z по направлению тиражирования
-   }
+   RotateVectorToVector(ReplicationDirection, SectionNormal);
+
    glGetFloatv(GL_MODELVIEW_MATRIX, Matrix); //получаем матрицу преобразований
-   Point3DFloatSet NewSectionPoints;
-   NewSectionPoints.ApplyChangesToPointsSet(Matrix);
-   Points[0] = NewSectionPoints;
-   //Points.push_back(NewSectionPoints);
-   Point3DFloatSet NewSectionNormals;
-   NewSectionNormals.ApplyChangesToVectorsSet(Matrix);
-   Normals[0] = NewSectionNormals;
-   //Normals.push_back(NewSectionNormals);
-   BorderNormals[0] = SectionNormal.ApplyChangesToVector(Matrix);
-   //glPopMatrix();
-    glLoadIdentity();
+
+   SectionPointsWithModifiedScale.ApplyChangesToPointsSet(Matrix);
+   Points[0] = SectionPointsWithModifiedScale;
+
+   SectionNormalVectorsWithModifiedScale.ApplyChangesToVectorsSet(Matrix);
+   NormalVectors[0] = SectionNormalVectorsWithModifiedScale;
+
+   BorderNormalVectors[0] = SectionNormal.ApplyChangesToVector(Matrix);
+
+   glPopMatrix();
 }
 
 void LastSection()
 {
-   //glPushMatrix(); //загрузка матрицы в стек
-   glTranslatef(ReplicationPath[ReplicationPath.size()-1].x, ReplicationPath[ReplicationPath.size() - 1].y, ReplicationPath[ReplicationPath.size() - 1].z); //перемещение в начало тиражирования
-   Point3DFloat ReplicationDirection = ReplicationPath[ReplicationPath.size() - 1] - ReplicationPath[ReplicationPath.size() - 2];  //направление тиражирования
+   glPushMatrix();
+
+   int num = Points.size() - 1;
+
+   Point3DFloatSet SectionPointsWithModifiedScale = SectionVertices;
+   Point3DFloatSet SectionNormalVectorsWithModifiedScale = SectionNormalVectors;
+   ModifyScale(SectionPointsWithModifiedScale, SectionNormalVectorsWithModifiedScale, Matrix, num);
+
+   glTranslatef(ReplicationPath[num].x, ReplicationPath[num].y, ReplicationPath[num].z); //перемещение в начало тиражирования
+
+   Point3DFloat ReplicationDirection = ReplicationPath[num] - ReplicationPath[num - 1];  //направление тиражирования
    Point3DFloat SectionNormal = Point3DFloat(0, 0, 1.); //нормаль
-   float angle = ReplicationDirection.GetAngle(SectionNormal);
-   Point3DFloat RotateDirection = SectionNormal.Cross(ReplicationDirection); //вектор,вокруг которого будем совершать поворот
-   if (RotateDirection.x > 1.0E-5 || RotateDirection.y > 1.0E-5 || RotateDirection.z > 1.0E-5)
-   {
-      RotateDirection.Normalize();
-      glRotatef(angle, RotateDirection.x, RotateDirection.y, RotateDirection.z);  //поворот, ось z по направлению тиражирования
-   }
+   RotateVectorToVector(ReplicationDirection, SectionNormal);
+
    glGetFloatv(GL_MODELVIEW_MATRIX, Matrix); //получаем матрицу преобразований
-   Point3DFloatSet NewSectionPoints;
-   NewSectionPoints.ApplyChangesToPointsSet(Matrix);
-   Points[Points.size() - 1] = NewSectionPoints;
-   //Points.push_back(NewSectionPoints);
-   /*Point3DFloatSet NewSectionNormals;
-   NewSectionNormals.ApplyChangesToVectorsSet(Matrix);
-   Normals.push_back(NewSectionNormals);*/
+
+   SectionPointsWithModifiedScale.ApplyChangesToPointsSet(Matrix);
+   Points[num] = SectionPointsWithModifiedScale;
+
    SectionNormal.z = -SectionNormal.z;
-   BorderNormals[1] = SectionNormal.ApplyChangesToVector(Matrix);
-   //glPopMatrix();
-   glLoadIdentity();
+   BorderNormalVectors[1] = SectionNormal.ApplyChangesToVector(Matrix);
+
+   glPopMatrix();
 }
 
 void Sections()
 {
-   for (int i = 1; i < ReplicationPath.size()-1; i++)
+   for (int i = 1; i < ReplicationPath.size() - 1; i++)
    {
-      //glPushMatrix(); //загрузка матрицы в стек
+      glPushMatrix();
+
+      Point3DFloatSet SectionPointsWithModifiedScale = SectionVertices;
+      Point3DFloatSet SectionNormalVectorsWithModifiedScale = SectionNormalVectors;
+      ModifyScale(SectionPointsWithModifiedScale, SectionNormalVectorsWithModifiedScale, Matrix, i);
+
       Point3DFloat PreviousReplicationDirection = ReplicationPath[i] - ReplicationPath[i - 1];  //направление тиражирования до узла
       Point3DFloat NextReplicationDirection = ReplicationPath[i + 1] - ReplicationPath[i];  //направление тиражирования после узла
       Point3DFloat SectionNormal = Point3DFloat(0, 0, 1.); //нормаль
       glTranslatef(ReplicationPath[i].x, ReplicationPath[i].y, ReplicationPath[i].z); //перемещение в узел
-      glPushMatrix();                                 
+      glPushMatrix();
       //для сечения
       Point3DFloat AddingDirection = NextReplicationDirection - PreviousReplicationDirection;
       AddingDirection.x /= 2.;
       AddingDirection.y /= 2.;
       AddingDirection.z /= 2.;
       Point3DFloat ReplicationDirection = PreviousReplicationDirection + AddingDirection;
-      float angle = ReplicationDirection.GetAngle(SectionNormal);
-      Point3DFloat RotateDirection = SectionNormal.Cross(ReplicationDirection); //вектор,вокруг которого будем совершать поворот
-      if (RotateDirection.x > 1.0E-5 || RotateDirection.y > 1.0E-5 || RotateDirection.z > 1.0E-5)
-      {
-         RotateDirection.Normalize();
-         glRotatef(angle, RotateDirection.x, RotateDirection.y, RotateDirection.z);  //поворот, ось z по направлению тиражирования
-      }
+
+      RotateVectorToVector(ReplicationDirection, SectionNormal);
+
       glGetFloatv(GL_MODELVIEW_MATRIX, Matrix); //получаем матрицу преобразований
-      Point3DFloatSet NewSectionPoints;
-      NewSectionPoints.ApplyChangesToPointsSet(Matrix);
-      Points[i] = NewSectionPoints;
-      //Points.push_back(NewSectionPoints);
-      /*Point3DFloatSet NewSectionNormals;
-      NewSectionNormals.ApplyChangesToVectorsSet(Matrix);
-      Normals.push_back(NewSectionNormals);*/
+
+      SectionPointsWithModifiedScale.ApplyChangesToPointsSet(Matrix);
+      Points[i] = SectionPointsWithModifiedScale;
+
       glPopMatrix();
       //для нормалей
       ReplicationDirection = NextReplicationDirection;
-      angle = ReplicationDirection.GetAngle(SectionNormal);
-      RotateDirection = SectionNormal.Cross(ReplicationDirection); //вектор,вокруг которого будем совершать поворот
-      if (RotateDirection.x > 1.0E-5 || RotateDirection.y > 1.0E-5 || RotateDirection.z > 1.0E-5)
-      {
-         RotateDirection.Normalize();
-         glRotatef(angle, RotateDirection.x, RotateDirection.y, RotateDirection.z);  //поворот, ось z по направлению тиражирования
-      }
+
+      RotateVectorToVector(ReplicationDirection, SectionNormal);
+
       glGetFloatv(GL_MODELVIEW_MATRIX, Matrix); //получаем матрицу преобразований
-      Point3DFloatSet NewSectionNormals;
-      NewSectionNormals.ApplyChangesToVectorsSet(Matrix);
-      Normals[i] = NewSectionNormals;
-      //Normals.push_back(NewSectionNormals);
-      glLoadIdentity();
+
+      SectionNormalVectorsWithModifiedScale.ApplyChangesToVectorsSet(Matrix);
+      NormalVectors[i] = SectionNormalVectorsWithModifiedScale;
+
+      glPopMatrix();
    }
 }
 
@@ -304,7 +325,7 @@ void DisplayGrid()
 
    glLineWidth(3);
 
-   
+
 
    glLineStipple(1, 255);
    glEnable(GL_LINE_STIPPLE);
@@ -315,7 +336,7 @@ void DisplayGrid()
 
    for (int i = 1; i < MaxCoord / GridStep; i++)
    {
-      glVertex2f(-MaxCoord, i* GridStep);
+      glVertex2f(-MaxCoord, i * GridStep);
       glVertex2f(MaxCoord, i * GridStep);
 
       glVertex2f(-MaxCoord, -i * GridStep);
@@ -336,19 +357,12 @@ void DisplayGrid()
    glEnd();
 
    glDisable(GL_LINE_STIPPLE);
-   //glVertex2f(-MaxCoord, 0.);
-   //glVertex2f(MaxCoord, 0.);
-
-   //glColor3ub(0, 0, 255);
-   //glVertex2f(0., -MaxCoord);
-   //glVertex2f(0., MaxCoord);
-   //glEnd;
 }
 
 void DisplaySections()
 {
-  // glColor3f(1., 0., 0.);
-   Point3DFloat Normal = BorderNormals[0];
+   glColor3ub(255., 255., 0.);
+   Point3DFloat Normal = BorderNormalVectors[0];
    glNormal3f(Normal.x, Normal.y, Normal.z);
    glBegin(GL_POLYGON);
    for (int i = 0; i < 5; i++)
@@ -359,12 +373,12 @@ void DisplaySections()
    glEnd();
    glNormal3f(0, 0, 1.);
 
-   Normal = BorderNormals[1];
+   Normal = BorderNormalVectors[1];
    glNormal3f(Normal.x, Normal.y, Normal.z);
    glBegin(GL_POLYGON);
    for (int i = 0; i < 5; i++)
    {
-      Point3DFloat Vertex = Points[Points.size()-1].Points[i];
+      Point3DFloat Vertex = Points[Points.size() - 1].Points[i];
       glVertex3f(Vertex.x, Vertex.y, Vertex.z);
    }
    glEnd();
@@ -372,15 +386,15 @@ void DisplaySections()
 
 void DisplayQuads()
 {
-   //glColor3f(.5, .5, .5);
+   glColor3ub(100, 100, 100);
    for (int i = 0; i < Points.size() - 1; i++)
    {
       Point3DFloatSet PreviousSection = Points[i];
-      Point3DFloatSet NextSection = Points[i+1];
+      Point3DFloatSet NextSection = Points[i + 1];
 
       for (int j = 0; j < 4; j++)
       {
-         Point3DFloat Normal = Normals[i].Points[j];
+         Point3DFloat Normal = NormalVectors[i].Points[j];
          Point3DFloat Vertices[4] = { PreviousSection.Points[j], PreviousSection.Points[j + 1], NextSection.Points[j + 1], NextSection.Points[j] };
 
          glNormal3f(Normal.x, Normal.y, Normal.z);
@@ -392,7 +406,7 @@ void DisplayQuads()
          }
          glEnd();
       }
-      Point3DFloat Normal = Normals[i].Points[4];
+      Point3DFloat Normal = NormalVectors[i].Points[4];
       Point3DFloat Vertices[4] = { PreviousSection.Points[4], PreviousSection.Points[0], NextSection.Points[0], NextSection.Points[4] };
 
       glNormal3f(Normal.x, Normal.y, Normal.z);
@@ -404,7 +418,7 @@ void DisplayQuads()
       }
       glEnd();
    }
-   
+
 }
 
 void Display(void)
@@ -413,8 +427,6 @@ void Display(void)
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    //glColor3ub(20,100);
 
-   
-   
    FirstSection();
    Sections();
    LastSection();
@@ -446,19 +458,17 @@ void main(int argc, char* argv[])
 {
    ReadSection();
    ReadReplicationPath();
-   SectionNormalsInit();
+   ReadScaleModifiers();
+   SectionNormalVectorsInit();
    Points.resize(ReplicationPath.size());
-   Normals.resize(Points.size() - 1);
+   NormalVectors.resize(Points.size() - 1);
    glutInit(&argc, argv);
    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
    glutInitWindowSize(Width, Height);
-   glutCreateWindow("Проекции");
+   glutCreateWindow("Тиражирование сечений");
    glutDisplayFunc(Display);
    glutReshapeFunc(Reshape);
    glEnable(GL_DEPTH_TEST);
-   //glEnable(GL_LIGHTING);
-   //glEnable(GL_LIGHT0);
-   //glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-   //glLightfv(GL_LIGHT0, GL_POSITION, light0_positionT);
+ 
    glutMainLoop();
 }
